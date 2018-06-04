544ea3c3b8e08c608126506e98ea399221b07e23
Sharon Mun <sharonymun@gmail.com>
2018-06-04 21:28:17 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -2,7 +2,7 @@ def unsafe?(speed)
 #below 40 or over 60 is true; 30 and 95 is unsafe. 50 is safe so return FALSE
 
 puts "30 and 95 are unsafe"
-if the_speed == 50
+if speed == 50
 
 speed < 40 || speed > 60 ? true : false
 
